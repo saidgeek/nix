ba0e66c5b260d4c74e4bce2520f759e1e8691713
saidgeek <me@saidgeek.land>
2023-07-08 08:44:38 -0400

    feat(home): add custom command to nulshell

--- programs/shell.nix
+++ programs/shell.nix
@@ -12,6 +12,15 @@
       home-reload = "home-manager switch --flake ${config.home.homeDirectory}/.dotfiles#saidgeek@l0kii";
       nixos-reload = "sudo nixos-rebuild switch --flake ${config.home.homeDirectory}/.dotfiles#l0kii";
     };
+    extraConfig = ''
+      let-env config = {
+        show_banner: false,
+      }
+
+      def gl [] {
+        git log --pretty=%h»¦«%aN»¦«%s»¦«%aD | lines | split column "»¦«" sha1 committer desc merged_at | first 10
+      }
+    '';
   };
 
   programs.zsh = {
